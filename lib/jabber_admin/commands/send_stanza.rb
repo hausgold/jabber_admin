616410a3aa30adb00ac899c552482104c50fcfef
Henning Vogt <git@henvo.de>
2018-01-29 12:03:11 +0100

    Fix #send_stanza comments

--- lib/jabber_admin/commands/send_stanza.rb
+++ lib/jabber_admin/commands/send_stanza.rb
@@ -3,8 +3,8 @@
 module JabberAdmin
   module Commands
     ##
-    # Password and Message can also be: none. Users JIDs are separated with :
-    # https://docs.ejabberd.im/developer/ejabberd-api/admin-api/#send-direct-invitation-send-a-direct-invitation-to-several-destinations
+    # Send a stanza; provide From JID and valid To JID
+    # https://docs.ejabberd.im/developer/ejabberd-api/admin-api/#send-stanza-send-a-stanza-provide-from-jid-and-valid-to-jid
     class SendStanza
       # @param [from] Sender JID
       # @param [to] Receiver JID
