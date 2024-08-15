0c46bdfee566b8bae761f861719eef0088ddc77a
deployhausgold <deploy@hausgold.de>
2024-08-15 12:56:20 +0000

    [Release] Bumped the version to 1.3.4.

--- lib/jabber_admin/version.rb
+++ lib/jabber_admin/version.rb
@@ -3,7 +3,7 @@
 # The gem version details.
 module JabberAdmin
   # The version of the +jabber_admin+ gem
-  VERSION = '1.3.3'
+  VERSION = '1.3.4'
 
   class << self
     # Returns the version of gem as a string.
